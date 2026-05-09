e418f96696fbdb327cc318348a415e3e6a8d9ab5
saikumar <saikumarlaveti5061@gmail.com>
2026-05-09 21:34:25 +0530

    spring data jpa

--- SpringDataJPACRUDOperations_06/src/main/java/com/Repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.Entity.Student;
-
-public interface StudentRepo extends CrudRepository<Student, Integer>{
-
-}
--- SpringDataJPACRUDOperations_06/src/main/java/com/Service/StudentService.java
+++
@@ -1,8 +0,0 @@
-package com.Service;
-
-import com.Entity.Student;
-
-public interface StudentService {
-	public String registerStudent(Student student);
-	public long countNumberOfStudent();
-}
