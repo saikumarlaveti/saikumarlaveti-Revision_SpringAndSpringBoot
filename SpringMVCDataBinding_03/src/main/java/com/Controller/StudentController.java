609ba8d16bdb2d28b2bd12366d6a1771dd060960
saikumar <saikumarlaveti5061@gmail.com>
2026-06-21 20:53:23 +0530

    data rendering

---
+++ SpringMVCDataBinding_03/src/main/java/com/Controller/StudentController.java
@@ -0,0 +1,34 @@
+package com.Controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.Entity.Student;
+
+@Controller
+public class StudentController {
+	
+	@GetMapping("/")
+	public String welcomePage() {
+		return "welcome";
+	}
+	
+	@GetMapping("/register")
+	public String showStudentFormPage() {
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String showResult(Map<String,Object> map,@ModelAttribute("stud") Student st) {
+		System.out.println(st);
+		
+		 System.out.println("POST Method Called");
+		return "show_result";
+	}
+	
+
+}
---
+++ SpringMVCDataBinding_03/src/main/java/com/Entity/Student.java
@@ -0,0 +1,15 @@
+package com.Entity;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+	public Integer id;
+	
+	public String name;
+	
+	public Integer age;
+	
+	public String gender;
+}
